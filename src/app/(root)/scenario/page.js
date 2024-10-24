312497da7bec127ed089babc61bdd4fa1c120256
ValerieBenedit <122312818+ValerieBenedit@users.noreply.github.com>
2024-10-23 22:45:00 -0400

    Update page.js
    finished product

--- src/app/(root)/scenario/page.js
+++ src/app/(root)/scenario/page.js
@@ -1,11 +1,35 @@
+import Image from 'next/image';
+import myImage1 from '/Users/Valerie/Desktop/Sophia&Mark.jpg';
+import myImage2 from '/Users/Valerie/Desktop/Jessica&Michael.jpg';
+import myImage3 from '/Users/Valerie/Desktop/Lina&David.jpg'; // Adjust the path as necessary
+
+const images = [
+  { src: myImage1, alt: 'Description of image 1' },
+  { src: myImage2, alt: 'Description of image 2' },
+  { src: myImage3, alt: 'Description of image 3' },
+];
+
+
 export default function Scenario() {
     return (
       <div className="container mx-auto p-6">
         <h1 className="text-4xl font-bold mb-4">Research</h1>
         <p className="text-lg">
-          This is the Scenario page. Content for this section will be added soon.
+          Scenarios for the Couples
         </p>
+        <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                {images.map((image, index) => (
+                    <div key={index} className="relative">
+                        <Image
+                            src={image.src}
+                            alt={image.alt}
+                            width={600} // Desired width
+                            height={400} // Desired height
+                            className="rounded-lg"
+                        />
+                    </div>
+                ))}
       </div>
+    </div>
     );
-  }
-  
+}
